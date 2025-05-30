0a829c72a0d7b98a2f80d9c8f3c4bd325136a42e
siddhant <isiddhantadhav@gmail.com>
2025-05-30 08:56:51 +0530

    sorting algos

--- sorting/sortingAlgo.cpp
+++ sorting/sortingAlgo.cpp
@@ -15,15 +15,32 @@ void selectionSort(vector<int> &arr) {
     }
 }
 
-void bubbleSort(vector<int> &arr){
+void bubbleSort(vector<int> &arr) {
     int n = arr.size();
 
-    bool swapped = false;
-     
+    for (int i=n-1; i>0; i--) {
+        bool swapped = false;
+        for (int j=0; j<i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) break; 
+    }
 }
 
-void insertionSort(vector<int> &arr){
 
+void insertionSort(vector<int> &arr){
+    int n = arr.size();
+
+    for(int i=0; i<n; i++){
+        int j=i;
+        while(j>0 && arr[j-1] > arr[j]){
+            swap(arr[j], arr[j-1]);
+            j--;
+        }
+    }
 }
 
 void mergeSort(vector<int> &arr){
@@ -53,7 +70,7 @@ int main() {
 
     vector<int> arr = {5,4,3,2,1};
 
-    bubbleSort(arr);
+    insertionSort(arr);
 
     for(auto i: arr){
         cout << i << " ";
