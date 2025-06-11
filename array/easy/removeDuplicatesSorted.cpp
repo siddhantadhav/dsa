a3de81e9f02f9777e7e493d8da9441f01c218afc
siddhant <isiddhantadhav@gmail.com>
2025-06-11 15:56:27 +0530

    started working with nvim

--- array/easy/removeDuplicatesSorted.cpp
+++ array/easy/removeDuplicatesSorted.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+int removeDuplicates(vector<int> &nums) {
+  int n = nums.size();
+  int k = 1;
+
+  for(int i=0; i<n; i++){
+    if(nums[i] == nums[k-1]) {
+    	nums[k] = nums[i];
+    	k++;
+    }
+  }
+
+  return k;
+}
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -13,3 +27,4 @@ int main() {
 
 	return 0;
 }
+
