c8b5bd21f7d53a11814e8b51768b1cbdfbe2a8a7
siddhant <isiddhantadhav@gmail.com>
2025-10-04 14:57:54 +0530

    restart dsa

--- main.cpp
+++ main.cpp
@@ -2,13 +2,6 @@
 
 using namespace std;
 
-void solve(int x, int y, int z){
-  if (y == 0 && x > z + 1)
-    cout << "No";
-  else
-    cout << "Yes";
-}
-
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
@@ -18,15 +11,9 @@ int main() {
     freopen("output.txt", "w", stdout);
   #endif
 
-  int t;
-  cin >> t;
-  
-  while(t--){
-    int x, y, z;
-    cin >> x >> y >> z;
 
-    solve(x, y, z);
-    cout << "\n";
-  }
+  cout << "Hello World" << "\n";
+  
   return 0;
 }
+
